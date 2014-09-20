106ba37e8acf8a1b0be980c730c0d71848756aad
Dart07 <danalind@gmail.com>
2014-09-20 10:42:07 -0500

    Update facebookfunct.js

--- facebookfunct.js
+++ facebookfunct.js
@@ -30,3 +30,29 @@ function getUserInfo() {
  
         });
    }  
+
+//Callback handler for response
+FB.Event.subscribe('auth.authResponseChange', function(response) 
+{
+     if (response.status === 'connected') 
+     {
+         document.getElementById("message").innerHTML +=  "<br>Connected to Facebook";
+        //SUCCESS
+     }   
+     else if (response.status === 'not_authorized') 
+    {
+        document.getElementById("message").innerHTML +=  "<br>Failed to Connect";
+        //FAILED
+    } else
+    {
+        document.getElementById("message").innerHTML +=  "<br>Unknown Error: Logged Out";
+        //UNKNOWN ERROR. Logged Out
+    }
+});
+
+//Logout --> should reload the current page it's on
+function Logout()
+{
+    FB.logout(function(){document.location.reload();});
+ 
+}
